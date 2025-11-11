2647d5fc40c042df720353d8ec127b33ade126e3
UnicornDad8 <cecibenitezca@gmail.com>
2025-11-11 14:14:38 -0300

    Add assets (icons and images) to project

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,6 +1,19 @@
+import { Outlet, Navigate } from "react-router-dom"
+
 const AuthLayout = () => {
+  const isAuthenticated = false;
+
   return (
-    <div>AuthLayout</div>
+    <>
+      { isAuthenticated ? <Navigate to="/" /> : 
+        <>
+          <section className="flex flex-col items-center justify-center flex-1 py-10">
+            <Outlet />
+          </section>
+          <img src="/assets/images/side-img.svg" alt="auth cover" className="hidden object-cover w-1/2 h-screen bg-no-repeat xl:block" />
+        </>
+      }
+    </>
   )
 }
 
